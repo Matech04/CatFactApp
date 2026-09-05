be4115a81642b46afcd27e903f37972915062977
Mateusz Chodacki <mateusz.chodacki@gmail.com>
2026-09-05 19:48:10 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿HttpClient httpClient = new();
+
+var response = await httpClient.GetAsync("https://catfact.ninja/fact");
+
+string jsonContent = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine(jsonContent);
